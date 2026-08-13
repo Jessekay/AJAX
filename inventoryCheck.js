f39ca5e6cf97660feaf1ce9529ebbba444738a4c
BAJIJI1 <kayjes00@gmail.com>
2026-08-13 15:36:30 +0200

    Implement inventory check functionality with legacy callback and promise support

---
+++ inventoryCheck.js
@@ -0,0 +1,35 @@
+function checkInventoryLegacy(itemId, quantity, callback) {
+  setTimeout(() => {
+    if (!itemId) {
+      callback("Invalid item ID provided!", null);
+    } else if (quantity > 10) {
+      callback(`Item ${itemId} out of stock for quantity ${quantity}!`, null);
+    } else {
+      callback(null, { itemId: itemId, quantity: quantity, pricePerUnit: 25 });
+    }
+  }, 1000);
+}
+
+function checkInventory(itemId, quantity) {
+  return new Promise((resolve, reject) => {
+    checkInventoryLegacy(itemId, quantity, (err, data) => {
+      if (err) {
+        reject(err)
+        return;
+      } else {
+        resolve(data);
+      }
+    });
+  }).then((data) => {
+    const totalCost = data.quantity * data.pricePerUnit;
+    return `Order confirmed for Item [itemId]! Total cost: ${totalCost}`
+    return transformedData;
+  }).catch((err) => {
+    console.error(err);
+    return fallbackData;
+  }).then((finalMessage) => {
+    console.log(finalMessage);
+  })
+}
+
+checkInventory('ITEM-101', 15);
