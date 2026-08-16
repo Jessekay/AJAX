d99dd90973aaf4e7233ed7e0a98ccf2294e5869f
BAJIJI1 <kayjes00@gmail.com>
2026-08-16 22:42:05 +0200

    Add fetchUser and fetchUserPosts functions with async/await support in loadDashboard

---
+++ fetchUser.js
@@ -0,0 +1,39 @@
+function fetchUser(userId) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (!userId) {
+        reject("User ID is required!");
+      } else {
+        resolve({ id: userId, username: "dev_jesse", role: "admin" });
+      }
+    }, 500);
+  });
+}
+
+function fetchUserPosts(role) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (role !== "admin") {
+        reject("Access denied: Admin role required to view posts.");
+      } else {
+        resolve(["Post 1: Async/Await Guide", "Post 2: Advanced Promises"]);
+      }
+    }, 500);
+  });
+}
+
+// Convert this function to async / await with try...catch
+async function loadDashboard(userId) {
+  try {
+    const user = await fetchUser(userId)
+    console.log(`User loaded: ${user.username}`);
+
+    const posts = await fetchUserPosts(user.role)
+    console.log(`Posts are loaded: ${user.posts}`);
+
+  } catch (err) {
+      console.error("Dashboard error:", err);
+  }
+}
+
+loadDashboard("USER-100")
