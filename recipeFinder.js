dbdfc350fda7f1de8c0c1d30c196587e0b8b1439
Jessekay <kayjes00@gmail.com>
2026-08-18 22:25:45 +0200

    Fix formatting and add newline at the end of recipeFinder.js

--- recipeFinder.js
+++ recipeFinder.js
@@ -1,7 +1,15 @@
 async function getRecipe() {
-  const recipe = await fetch('/api/recipe/42')
-  const data = await recipe.json()
+  const recipe = await fetch('/api/recipe/42');
+  const data = await recipe.json();
   console.log(data);
   return data;
-  
+}
+
+async function getRecipeChained() {
+  return fetch('/api/recipe/42')
+  .then(r => r.json())
+  .then(data => {
+    console.log(data);
+    return data;
+  });
 }
