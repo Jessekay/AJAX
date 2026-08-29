90433a7e46f2f42725c116dac4609e32a4162abc
Jessekay <kayjes00@gmail.com>
2026-08-29 23:27:01 +0200

    Implement error handling and result aggregation in fetchMultipleAPIs function

--- posts.js
+++ posts.js
@@ -8,6 +8,19 @@ async function fetchMultipleAPIs(apiUrls) {
       }
     })
   );
+
+  const settledResults = await Promise.allSettled(fetchPromises);
+
+  const results = [];
+
+  settledResults.forEach((result, index) => {
+    if(result.status === 'fulfilled') {
+      results.push(result.value)
+    } else {
+      console.error(`Failed to fetch ${apiUrls[index]}`, result.reason);
+    }
+  });
+  return results;
 }
 
 
@@ -16,4 +29,12 @@ const apiUrls = [
   'https://jsonplaceholder.typicode.com/posts/5',
   'https://jsonplaceholder.typicode.com/posts/6',
 ];
+
+fetchMultipleAPIs(apiUrls)
+  .then(results => {
+    console.log('Combined Results:', results);
+  })
+  .catch(error => {
+    console.log('Error:', error.message);
+  });
 
