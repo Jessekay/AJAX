7fdfdbe9b2dc7ca404299b44fe859b82f1480291
Jessekay <kayjes00@gmail.com>
2026-09-09 11:52:25 +0200

    Fix orderPizza function to return specific pizza name and adjust console log messages for clarity

--- pizzaOrder.js
+++ pizzaOrder.js
@@ -1,21 +1,45 @@
 function orderPizza(pizza) {
   return new Promise((resolve, reject) => {
-    const success = false;
+    const success = true;
     setTimeout(() => {
       if(success) {
-        resolve('Pizza is ready!')
+        resolve(`${pizza} is ready!`)
       } else {
         reject('Not available')
       }
-    }, 1500);
+    }, 1000);
   });
+}
+
+function deliverPizza(order) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`${order} has been delivered!`)
+    }, 1000)
+  });
+}
+
+function eatPizza(delivery) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`${delivery} and now it is eaten!`)
+    }, 1000);
+  })
 }
 
 orderPizza('pepperoni')
 .then((order) => {
-  console.log(`The order is placed:`, order);
+  console.log('step 1:', order);
+  return deliverPizza(order)
+})
+.then((delivered) => {
+  console.log('step 2:', delivered);
+  return eatPizza(delivered);
+})
+.then((eaten) => {
+  console.log('step 3:', eaten);
 })
 .catch((error) => {
   console.log('Something went wrong', error);
-  
 });
+
