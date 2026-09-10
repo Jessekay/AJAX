3bd84e0a5f7a057885d5ce6e1b7768df3cf3cde5
Jessekay <kayjes00@gmail.com>
2026-09-10 10:30:15 +0200

    Refactor all.js to replace primitive values with promises for better error handling in Promise.all

--- all.js
+++ all.js
@@ -1,7 +1,17 @@
-const promise1 = 89;
-const p2 = 'Jesse';
-const p3 = true;
+const p1 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 2000, 'step 1');
+})
 
-Promise.all([promise1, p2, p3]).then((values) => {
+const p2 = new Promise((resolve, reject) => {
+  setTimeout(reject, 3000, 'step 2 failed');
+})
+
+const p3 = new Promise((resolve, reject) => {
+  setTimeout(reject, 2000, 'step 3 has failed');
+});
+
+Promise.all([p1, p2, p3]).then((values) => {
   console.log(values);
+}).catch((err) => {
+  console.error(err);
 })
