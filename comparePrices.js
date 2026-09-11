cb7df1c367d1f223a1016164d746e256c8fa1c7a
Jessekay <kayjes00@gmail.com>
2026-09-11 14:51:48 +0200

    Add pairing logic to fetchAllPrices function to associate product IDs with their fetched results

--- comparePrices.js
+++ comparePrices.js
@@ -2,4 +2,7 @@ async function fetchAllPrices(productIds, fetchPriceFn) {
   const results = await Promise.allSettled(
     productIds.map(id => fetchPriceFn(id))
   );
+  const paired = productIds.map((id, index) => {
+    return { id: id, result: results[index] };
+  })
 }
