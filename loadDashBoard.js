cf9c9886514e328a42e81efd93067b8a04779bdc
Jessekay <kayjes00@gmail.com>
2026-09-18 11:48:50 +0200

    Add logging for response status and ok flag in loadDashboard function

--- loadDashBoard.js
+++ loadDashBoard.js
@@ -1,8 +1,14 @@
 async function loadDashboard() {
   try {
     const [user, todos] = await Promise.all([
-    fetch('https://jsonplaceholder.typicode.com/api/user').then(res => res.json()),
-    fetch('https://jsonplaceholder.typicode.com/api/todos').then(res => res.json())
+    fetch('https://jsonplaceholder.typicode.com/api/user').then(res => {
+      console.log(res.status, res.ok);
+      res.json()
+    }),
+    fetch('https://jsonplaceholder.typicode.com/api/todos').then(res => {
+      console.log(res.status, res.ok);
+      res.json()
+    })
   ]);
     return {
       user, todos 
