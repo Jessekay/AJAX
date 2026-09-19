93c60030df74985112866af9ed943af84e95ecd4
Jessekay <kayjes00@gmail.com>
2026-09-20 00:58:45 +0200

    Implement debounce function and update input event handling in debounce.js

--- debounce.js
+++ debounce.js
@@ -6,3 +6,11 @@ const throttleText = document.getElementById('throttle');
 input.addEventListener("input", e => {
   defaultText.textContent = e.target.value;
 })
+
+const updatedText = debounce((text) => {
+  defaultText.textContent = text;
+})
+
+function debounce(cb, delay = 1000) {
+
+}
